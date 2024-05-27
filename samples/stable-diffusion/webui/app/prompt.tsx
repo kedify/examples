5d519e4429641077a997f494bee00f7180fbeb84
Jirka Kremser <jiri.kremser@gmail.com>
2024-05-27 13:34:11 +0200

    Mostly UI tweaks
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- samples/stable-diffusion/webui/app/prompt.tsx
+++ samples/stable-diffusion/webui/app/prompt.tsx
@@ -6,44 +6,49 @@ import { Flip, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface PromptProps {
-  sendToQ: (img: string) => Promise<void>;
+  sendToQ: (img: string, count: number) => Promise<void>;
 }
 export const Prompt: React.FC<PromptProps> = ({ sendToQ }) => {
 
   const [prompt, setPrompt] = useState('')
-  const mutation = useMutation({
-    mutationFn: (img: string) => {
-      return sendToQ(img);
+  const [count, setCount] = useState(1)
+  const mutation = useMutation<any, any, [string, number]>({
+    mutationFn: ([img, count]: [string, number]) => {
+      return sendToQ(img, count);
     },
   });
   const generateOnClick = () => {
     if (prompt) {
       toast.success("Task has been added to job queue 🤖");
-      mutation.mutate(prompt);
+      mutation.mutate([prompt, count]);
       setPrompt('');
+      setCount(1);
     }
   }
 
   return (
-    <div className="flex items-left">
-      <div className="pb-6 pt-8 mr-2">Prompt:</div>
-      <input className="backdrop-blur-2xl dark:border-neutral-800  dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-100 lg:p-4 lg:dark:bg-zinc-800/30"
-          name="prompt" placeholder='Yellow submarine' value={prompt} onChange={e => setPrompt(e.target.value)}>
-      </input>
-      <button className="ml-5 bg-transparent border border-slate-200 hover:bg-slate-800 dark:border-slate-700 dark:text-slate-100 p-5" onClick={generateOnClick}>Generate</button>
-      <ToastContainer 
-        position="top-right"
-        autoClose={7000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-        transition={Flip}
-      />
+    <div className="flex flex-col gap-5">
+      <div className="flex items-left">
+        <div className="pt-5 mr-2">Prompt:</div>
+        <input className="backdrop-blur-2xl dark:from-inherit static w-auto rounded-xl border bg-gray-100 p-4 dark:bg-zinc-800/30 prompt"
+            name="prompt" placeholder='Yellow submarine' value={prompt} onChange={e => setPrompt(e.target.value)}>
+        </input>
+        <button className="ml-5 bg-transparent border border-slate-200 rounded-xl dark:border-slate-700 dark:text-slate-100 p-4 generate" onClick={generateOnClick}>Generate</button>
+        <ToastContainer 
+          position="top-right"
+          autoClose={7000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Flip}
+        />
+      </div>
+      Number of images: {count} <input type="range" max="4" min="1" value={count} onChange={e => setCount(+e.target.value)} className="text-slate-100"/>
     </div>
   )
 }
