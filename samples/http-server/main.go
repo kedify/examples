769a4d659f6d8ec9f848fed05559f15c95a17b8a
Zbynek Roubalik <zroubalik@gmail.com>
2025-01-24 22:08:19 +0100

    http-server app: add Prom metric
    Signed-off-by: Zbynek Roubalik <zroubalik@gmail.com>

--- samples/http-server/main.go
+++ samples/http-server/main.go
@@ -7,12 +7,32 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	// Define a counter metric for the root and image handlers
+	requests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of requests to the handlers.",
+		},
+		[]string{"endpoint"},
+	)
+)
+
+func init() {
+	// Register custom metrics with Prometheus
+	prometheus.MustRegister(requests)
+}
 
 func main() {
 	delay := getDelay()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requests.WithLabelValues("/").Inc() // Increment the counter for root endpoint
 		fmt.Println("Received request from", r.RemoteAddr)
 		time.Sleep(delay)
 		w.Header().Set("Content-Type", "text/html")
@@ -40,9 +60,13 @@ func main() {
 	})
 
 	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
+		requests.WithLabelValues("/image").Inc() // Increment the counter for image endpoint
 		time.Sleep(delay)
 		http.ServeFile(w, r, "kedify-loves-keda.gif")
 	})
+
+	// Expose the default Prometheus metrics at `/metrics` endpoint
+	http.Handle("/metrics", promhttp.Handler())
 
 	fmt.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
