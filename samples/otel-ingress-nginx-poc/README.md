fcc7ae2bfdab8d8d2e68685b30f3206937707878
Jirka Kremser <jiri.kremser@gmail.com>
2026-06-30 17:52:16 +0200

    make copilot happy
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- samples/otel-ingress-nginx-poc/README.md
+++ samples/otel-ingress-nginx-poc/README.md
@@ -1,25 +1,43 @@
 # Setup
-1. expose the ingress-nginx metrics endpoint
+1. install the Apollo Router backend
 
 ```
-helm upgrade -i nginx-ingress nginx-ingress --reuse-values \
-   https://kubernetes.github.io/ingress-nginx \
+helm upgrade --install my-app-be \
+   --namespace app \
+   --create-namespace \
+   oci://ghcr.io/apollographql/helm-charts/router \
+   -f ./apollo-values.yaml
+```
+
+2. expose the ingress-nginx metrics endpoint
+
+```
+helm upgrade --install ingress-nginx ingress-nginx \
+   --repo https://kubernetes.github.io/ingress-nginx \
+   --reuse-values \
    --namespace ingress-nginx \
+   --create-namespace \
    --set controller.metrics.enabled=true
 ```
 
-2. install the OTel Scaler
+3. install the OTel Scaler
+
 ```
-helm upgrade -i keda-otel-scaler -nkeda \
- oci://ghcr.io/kedify/charts/otel-add-on \
- --version=v0.1.3 \
- -f ./otel-scaler-values.yaml
+helm upgrade --install keda-otel-scaler \
+   --namespace keda \
+   --create-namespace \
+   oci://ghcr.io/kedify/charts/otel-add-on \
+   --version=v0.1.3 \
+   -f ./otel-scaler-values.yaml
 ```
 
-3. create scaled objects
+4. create the demo app and ScaledObjects
 
+```
+kubectl apply -f fe.yaml -f be-so.yaml -f fe-so.yaml
+```
 
 # Architecture
 ![diagram](./diagram.png "Diagram")
 
-[## https://excalidraw.com/#json=bMb11limAWMauy_ChBZFb,D-XdxbUT9BR6ZPcLszl5Fw](https://excalidraw.com/#json=VzaunOMs2NXOd_rTVy2yl,RU1vtVumsMIw-PxaUETKKw)
+[Source diagram](https://excalidraw.com/#json=VzaunOMs2NXOd_rTVy2yl,RU1vtVumsMIw-PxaUETKKw)
--- samples/otel-ingress-nginx-poc/setup.sh
+++ samples/otel-ingress-nginx-poc/setup.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-set -e
-k apply -f https://raw.githubusercontent.com/prometheus-community/helm-charts/refs/heads/main/charts/kube-prometheus-stack/charts/crds/crds/crd-servicemonitors.yaml
-helm upgrade -i my-app-be \
-   --namespace other-app \
+set -euo pipefail
+
+helm upgrade --install my-app-be \
+   --namespace app \
    --create-namespace \
    oci://ghcr.io/apollographql/helm-charts/router \
-   -f apollo-values.yaml
+   -f ./apollo-values.yaml
 
-helm upgrade -i ingress-nginx ingress-nginx --reuse-values \
-   https://kubernetes.github.io/ingress-nginx \
+helm upgrade --install ingress-nginx ingress-nginx \
+   --repo https://kubernetes.github.io/ingress-nginx \
+   --reuse-values \
    --namespace ingress-nginx \
+   --create-namespace \
    --set controller.metrics.enabled=true
 
-#k port-forward svc/ingress-nginx-controller-metrics 10254 -ningress-nginx
+# kubectl -n ingress-nginx port-forward svc/ingress-nginx-controller-metrics 10254
 #open http://localhost:10254/metrics
 
 # nginx_ingress_controller_connect_duration_seconds_count{canary="",controller_class="k8s.io/ingress-nginx",controller_namespace="ingress-nginx",controller_pod="ingress-nginx-controller-d85848749-8z66x",host="my-app-fe.local",ingress="my-app-fe",method="GET",namespace="app",path="/",service="my-app-fe",status="200"} 1
 
-# k port-forward -ningress-nginx service/ingress-nginx-controller 8080:80
+# kubectl -n ingress-nginx port-forward service/ingress-nginx-controller 8080:80
 # curl --resolve my-app-fe.local:8080:127.0.0.1 http://my-app-fe.local:8080
 
-helm upgrade -i keda-otel-scaler -nkeda \
- oci://ghcr.io/kedify/charts/otel-add-on \
- --version=v0.1.3 \
- -f ./otel-scaler-values.yaml
+helm upgrade --install keda-otel-scaler \
+   --namespace keda \
+   --create-namespace \
+   oci://ghcr.io/kedify/charts/otel-add-on \
+   --version=v0.1.3 \
+   -f ./otel-scaler-values.yaml
 
-k apply -f fe.yaml -f be-so.yaml -f fe-so.yaml
+kubectl apply -f fe.yaml -f be-so.yaml -f fe-so.yaml
 
 echo -e "continue w/:\nhey -c 100 -z 60s -t 90 -host my-app-fe.local http://localhost:8080"
 #hey -c 100 -z 60s -t 90 -host my-app-fe.local http://localhost:8080
